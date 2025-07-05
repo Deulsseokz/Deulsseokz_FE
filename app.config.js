8fc1b2f940c3348b7dccda14e06d9823ed76439c
yminjuu <ymj0580@naver.com>
2025-07-05 16:04:42 +0900

    chore: 지도 api 라이브러리 세팅

---
+++ app.config.js
@@ -0,0 +1,82 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "Mellog",
+    slug: "mellog",
+    version: "1.0.0",
+    owner: "patrickgong",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "mellog",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      bundleIdentifier: "com.mellog.deulseokzz",
+      supportsTablet: false,
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription:
+          "이 앱은 현재 위치를 기반으로 지도를 표시하기 위해 위치 권한이 필요합니다.",
+        NSLocationAlwaysUsageDescription:
+          "항상 위치 권한을 요청하는 이유를 설명합니다. (필요한 경우에만 사용)",
+      },
+    },
+    android: {
+      package: "com.mellog.deulseokzz",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+      permissions: ["ACCESS_FINE_LOCATION", "ACCESS_COARSE_LOCATION"],
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "@mj-studio/react-native-naver-map",
+        {
+          client_id: process.env.NAVER_MAP_CLIENT_ID,
+          android: {
+            ACCESS_FINE_LOCATION: true,
+            ACCESS_COARSE_LOCATION: true,
+          },
+          ios: {
+            NSLocationWhenInUseUsageDescription:
+              "이 앱은 지도를 표시하기 위해 위치 권한이 필요합니다.",
+          },
+        },
+      ],
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+      [
+        "expo-build-properties",
+        {
+          android: {
+            extraMavenRepos: ["https://repository.map.naver.com/archive/maven"],
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "0b8c439b-7629-447f-a13a-e3985fe9703f",
+      },
+    },
+  },
+};
---
+++ app/(tabs)/mypage.tsx
@@ -0,0 +1,43 @@
+import { NaverMapView } from "@mj-studio/react-native-naver-map";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
+import { Alert, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const INITIAL_CAMERA = {
+  latitude: 37.5666102,
+  longitude: 126.9783881,
+  zoom: 12,
+  latitudeDelta: 0.38,
+  longitudeDelta: 0.8,
+};
+
+const MountainMapScreen = () => {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      console.log("권한 상태", status);
+
+      if (status !== "granted") {
+        Alert.alert("위치 권한이 필요합니다.");
+        setHasPermission(false);
+        return;
+      }
+      setHasPermission(true);
+    })();
+  }, []);
+
+  if (hasPermission === false) {
+    return <SafeAreaView className="flex-1 bg-white" />;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <NaverMapView style={{ flex: 1 }} initialCamera={INITIAL_CAMERA} />
+    </View>
+  );
+};
+
+export default MountainMapScreen;
