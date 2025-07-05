4ebc62190281fab324287f493703a53f5a13748b
kweonsikyung <ekrk6632@naver.com>
2025-07-06 04:08:57 +0900

    feat: implement font system

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,7 +9,9 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    'Pretendard-Regular': require('../assets/fonts/Pretendard-Regular.ttf'),
+    'Pretendard-Bold': require('../assets/fonts/Pretendard-Bold.ttf'),
+    'Pretendard-SemiBold': require('../assets/fonts/Pretendard-SemiBold.ttf'),
   });
 
   if (!loaded) {
