0aab5e94b45e165d6a78c4ad6d0702d78f0020f7
yminjuu <ymj0580@naver.com>
2025-07-17 02:50:17 +0900

    feat: locationheader implement

--- components/map/LocationHeader.tsx
+++ components/map/LocationHeader.tsx
@@ -1,6 +1,55 @@
+import IcnClose from '@/assets/icons/icon-close-black.svg';
+import IcnSearch from '@/assets/icons/icon-search.svg';
+import { MCOLORS } from '@/constants/Colors';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface LocationHeaderProps {
+  title: string;
+  onSearch: () => void;
+  onClose: () => void;
+}
+
 /**
  * @returns 지역 스크린을 이루는 헤더
  */
-export default function LocationHeader() {
-  return <></>;
+export default function LocationHeader({ title, onSearch, onClose }: LocationHeaderProps) {
+  return (
+    <View style={style.container}>
+      <Text style={style.title}>{title}</Text>
+      <View style={style.icnContainer}>
+        <TouchableOpacity onPress={onSearch}>
+          <IcnSearch style={style.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <IcnClose style={style.icon} onPress={onClose} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    color: MCOLORS.grayscale.gray80,
+    fontSize: 24,
+    fontFamily: 'Pretendard-Bold',
+    fontWeight: '700',
+    lineHeight: 28,
+  },
+  icnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: 15,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
--- components/template/map/LocationTemplate.tsx
+++ components/template/map/LocationTemplate.tsx
@@ -1,4 +1,26 @@
+import LocationHeader from '@/components/map/LocationHeader';
+import LocationList from '@/components/map/LocationList';
+import LocationSelector from '@/components/map/LocationSelector';
+import { router } from 'expo-router';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 // 지역 선택 화면을 이루는 기본 골격
-export default function BottomSheetTemplate() {
-  return <></>;
+export default function LocationTemplate() {
+  return (
+    <SafeAreaView style={style.container}>
+      <LocationHeader title="지역" onClose={() => router.back()} onSearch={() => router.push('/test')} />
+      <LocationSelector />
+      <LocationList />
+    </SafeAreaView>
+  );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 22,
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff',
+  },
+});
