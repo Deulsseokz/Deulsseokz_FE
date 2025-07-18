b6fab87888cf32e6a41135344f5c6a92f7d098f3
kweonsikyung <ekrk6632@naver.com>
2025-07-17 17:09:10 -0700

    feat: API call to fetch album list

--- app/(tabs)/album/index.tsx
+++ app/(tabs)/album/index.tsx
@@ -1,41 +1,27 @@
+import { getAlbumList } from "@/api/album";
+import { AlbumItem } from "@/api/type";
 import AlbumHomeTemplate from "@/components/template/AlbumHomeTemplate";
-
-export interface IAlbum {
-  id: string;
-  label: string;
-  images: number[]; 
-}
-
-const albums : IAlbum[] = [
-  {
-    id: "1",
-    label: "N서울타워",
-    images: [
-      require("@/assets/images/album/photo1.jpeg"),
-      require("@/assets/images/album/photo2.jpeg"),
-      require("@/assets/images/album/photo3.jpeg"),
-    ],
-  },
-  {
-    id: "2",
-    label: "코엑스 아쿠아리움",
-    images: [
-      require("@/assets/images/album/photo3.jpeg"),
-      require("@/assets/images/album/photo1.jpeg"),
-      require("@/assets/images/album/photo2.jpeg"),
-    ],
-  },
-  {
-    id: "3",
-    label: "해운대",
-    images: [
-      require("@/assets/images/album/photo2.jpeg"),
-      require("@/assets/images/album/photo3.jpeg"),
-      require("@/assets/images/album/photo1.jpeg"),
-    ],
-  },
-];
+import { useEffect, useState } from "react";
 
 export default function AlbumHomeScreen() {
+  const [albums, setAlbums] = useState<AlbumItem[]>([]);
+
+  const fetchAlbums = async () => {
+      try {
+        const res = await getAlbumList();
+        if (res.isSuccess) {
+          setAlbums(res.result);
+        } else {
+          console.error("API 오류:", res.message);
+        }
+      } catch (e) {
+        console.error("API 호출 실패:", e);
+      }
+    };
+
+  useEffect(() => {
+    fetchAlbums();
+  }, []);
+
   return <AlbumHomeTemplate title="앨범" albums={albums} />;
 }
