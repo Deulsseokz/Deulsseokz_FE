0aff68aafd5f785459579b5ceae65a49c7678876
Patrick-Gong <owlcountry01@gmail.com>
2025-11-06 18:42:05 +0900

    feat: 로그인과 미션 제출 시에 에러 발생할 때 보여줄 alert 추가

--- app/(app)/map/[id]/index.tsx
+++ app/(app)/map/[id]/index.tsx
@@ -1,10 +1,11 @@
 import api from '@/api/client';
 import ChallengeDetailTemplate from '@/components/template/ChallengeDetailTemplate';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getMessaging, hasPermission } from '@react-native-firebase/messaging';
 import dayjs from 'dayjs';
 import { router, useLocalSearchParams } from 'expo-router';
 import { useState } from 'react';
-import { Alert } from 'react-native';
+import { Alert, Linking, Platform } from 'react-native';
 const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
 
 export default function ChallengeDetail() {
@@ -13,7 +14,7 @@ export default function ChallengeDetail() {
   const uriParts = image as string;
   const fileType = uriParts.split('.')[uriParts.split('.').length - 1];
   const formData = new FormData();
-
+  const messaging = getMessaging();
   formData.append('place', placeName as string);
   if (typeof friends === 'string') {
     formData.append('friends', JSON.stringify(friends.split(',').map(Number)));
@@ -30,6 +31,22 @@ export default function ChallengeDetail() {
   const handleSubmit = async () => {
     setIsLoading(true);
     try {
+      const authStatus = await hasPermission(messaging);
+      console.log('authStatus', authStatus);
+      if (!authStatus) {
+        Alert.alert(
+          '알림 권한 설정',
+          Platform.OS === 'ios'
+            ? '알림 권한을 허용해야만 챌린지에 도전할 수 있습니다.\n설정 > 앱 > Mellog > 알림 > 허용 후 리부팅해주세요.'
+            : '알림 권한을 허용해야만 챌린지에 도전할 수 있습니다.\n설정 > 애플리케이션 > Mellog > 알림 > 허용 후 리부팅해주세요.',
+          [
+            { text: '설정 열기', onPress: () => Linking.openSettings() },
+            { text: '닫기', style: 'cancel' },
+          ],
+        );
+        return;
+      }
+
       const res = await api.post(`${BASE_URL}/challenge/`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
@@ -45,11 +62,12 @@ export default function ChallengeDetail() {
         AsyncStorage.setItem('image', image as string);
         AsyncStorage.setItem('attemptId', String(res.data.result.attemptId));
       }
+      Alert.alert('챌린지 심사 중', 'AI가 사진을 분석하고 있습니다. 분석이 끝나면 푸시 알림으로 알려드릴게요!');
     } catch (error) {
       console.log('챌린지 도전 error', error);
+      Alert.alert('챌린지 도전 실패', 'owlcountry01@gmail.com으로 문의해주세요.');
     } finally {
       setIsLoading(false);
-      Alert.alert('챌린지 심사 중', 'AI가 사진을 분석하고 있습니다. 분석이 끝나면 푸시 알림으로 알려드릴게요!');
       router.back();
     }
   };
